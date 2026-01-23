8155e66d839202edda35066af873500c4059abb6
Abdullah Waleed <idevenk01@gmail.com>
2026-01-23 18:20:12 +0200

    Create Check Equal Arrays.cpp

---
+++ Check Equal Arrays.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        // code here
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        return a == b;
+    }
+};
